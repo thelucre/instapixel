fbe31007af645d4e64e9c70d738c180fe47c463c
Eric Howard <erichoward86@gmail.com>
2013-08-10 15:07:37 -0400

    added main pixel processing logic. first working version

--- jquery.instapixel.js
+++ jquery.instapixel.js
@@ -3,7 +3,7 @@
  Author: 	Eric Howard
  URL: 		http://www.thelucre.com
  GitHub:	https://github.com/thelucre/instapixel
- Veriosn:	v0.0.4	  
+ Version:	v0.0.5	  
 
  Settings:
  - 'debug' 							[true] false, will output information/errors to the console
@@ -23,14 +23,14 @@
 	$.fn.instapixel = function( options ) {
 		// CONSTANTS
 		var INSTAPIXEL = 'InstaPIXEL'
-		,	MSG_NOT_CANVAS_ELEMENT = 'The element is not of type <CANVAS>'
-		,	MSG_TYPE_ERROR = 'ERROR'
-		,	MSG_TYPE_INFO = 'INFO'
-		,	MSG_INSTAPIXEL_STARTED = "InstaPIXEL plugin started on element."
-		,	MSG_IMAGE_LOAD_ATTEMPT = 'Attempting to load image.'
-		,	MSG_IMAGE_LOAD_ERROR = 'Image could not be loaded.'
-		,	MSG_IMAGE_LOADED = 'Image successfully loaded.'
-		,	MSG_CANVAS_NOT_SUPPORTED = 'The <CANVAS> element is not supported in this browser.';
+		,		MSG_TYPE_ERROR = 'ERROR'
+		,		MSG_TYPE_INFO = 'INFO'
+		,		MSG_NOT_CANVAS_ELEMENT = 'The element is not of type <CANVAS>'
+		,		MSG_INSTAPIXEL_STARTED = "InstaPIXEL plugin started on element."
+		,		MSG_IMAGE_LOAD_ATTEMPT = 'Attempting to load image.'
+		,		MSG_IMAGE_LOAD_ERROR = 'Image could not be loaded.'
+		,		MSG_IMAGE_LOADED = 'Image successfully loaded.'
+		,		MSG_CANVAS_NOT_SUPPORTED = 'The <CANVAS> element is not supported in this browser.';
 
 		// DEFAULT SETTINGS
 		var defaults = {
@@ -41,11 +41,13 @@
  		,	'resizeCanvas': 			'true' 
 		};
 		var options = $.extend({}, defaults, options); 
-		var imageURL			// stores the loaded image
+		var image 				// stores the loaded image
 		,	ctx 						// ctx of the displayed canvas
 		,	canv 						// displayed canvas element
 		,	tmpcanv 				// buffer canvas for reading pixels
-		,	tmpctx; 				// buffer context for reading pixels 
+		,	tmpctx	 				// buffer context for reading pixels 
+		,	pix 						// holds pixel data for the current image
+		;
 		var self = this; 	// globalized within plugin scope
 
 		message( MSG_TYPE_INFO, MSG_INSTAPIXEL_STARTED, this.selector);
@@ -56,7 +58,76 @@
 
 		canv = this[0];
 		ctx = canv.getContext('2d');
+		tmpcanv = this.clone()[0];
+		tmpctx = tmpcanv.getContext('2d');
 		loadImage(options.imageURL);
+
+		// MAIN PIXEL PROCESSING
+		/*********************************************
+		 * loads the pix vairable with pixel data from 
+		 * the buffered canvimg canvas
+		 *********************************************/
+		function getPixelDataArray( ) {
+			
+			var imageData = tmpctx.getImageData(0, 0, image.width, image.height);
+		  var data = imageData.data;
+	    pix = { };
+	    // iterate over all pixels based on x and y coordinates
+	    for(var y = 0; y < image.height; y++) {
+	      pix[y] = { };
+	      // loop per row index
+	      for(var x = 0; x < image.width; x++) {
+	        var red = data[((image.width * y) + x) * 4];
+	        var green = data[((image.width * y) + x) * 4 + 1];
+	        var blue = data[((image.width * y) + x) * 4 + 2];
+	        // push onto array
+	        pix[y][x] = 'rgb(' + red + ',' + green + ',' + blue + ')';	
+	      }
+	    }
+		} // end getPixelDataArray()
+
+		function drawPixelatedToCanvas() {
+			// amount to scale each pixel drawn for cnavas size
+			var canvScale = canv.width / tmpcanv.width; // 0.490196078
+
+			var tileSize = options.startingPixelSize;
+
+			// size of the tile to be drawn
+			var canvTile = tileSize * canvScale;
+
+			// number of pixels in row or column (scaled for canvas size)
+			var tiles = { 'x': Math.floor(tmpcanv.width / canvTile)	
+			  		  	  ,	'y': Math.floor(tmpcanv.height / canvTile) };
+
+			// pad values used to make the number of square tiles even given any size canvas
+			// so there will appear to be a perfect fit for all squares drawn
+			var pad = { 'x': canv.width % canvTile
+			  				,	'y': canv.height % canvTile };	// the total extra space on the canvas
+
+			var padamt = {	'x' : pad.x / tiles.x
+						   			,	'y' : pad.y / tiles.y };			// the padding per square that will be added to both sides
+
+			for(var x = 0; x < tiles.x; x++) {
+			    for(var y = 0; y < tiles.y; y++) { 
+			    //console.log(x + ", " + y);
+					var colorCoords = { 'x': Math.floor(x * tileSize + tileSize / 2)
+									 					,	'y': Math.floor(y * tileSize + tileSize / 2) };
+
+					if(colorCoords.x <= 0 ) colorCoords.x = 1;
+					if(colorCoords.y <= 0) colorCoords.y = 1;
+					if(colorCoords.x > 612 -1 ) colorCoords.x = 612 -1;
+					if(colorCoords.y > 612 -1) colorCoords.y = 612 -1;
+					console.log(colorCoords.x + " , " + colorCoords.y);{}
+					if(pix[colorCoords.y] && pix[colorCoords.y][colorCoords.x]) {
+				        ctx.fillStyle = pix[colorCoords.y][colorCoords.x];
+								ctx.fillRect(	Math.floor(x * canvTile + x * padamt.x), 
+										Math.floor(y * canvTile + y * padamt.y), 
+										Math.floor((x * canvTile + x * padamt.x ) + canvTile + padamt.x),
+										Math.floor((y * canvTile ) + canvTile + padamt.y ));
+					}
+				}
+			}
+		}
 
 		// UTILITY FUNCTIONS
 		function loadImage( imgurl ) {
@@ -71,10 +142,12 @@
 					this.onerror();
 					return;
 				}
-				resizeCanvas();
-				ctx.drawImage(this, 0, 0, canv.width, canv.height);
+				resizeCanvases();
+				tmpctx.drawImage(this, 0, 0, canv.width, canv.height);
 				message( MSG_TYPE_INFO, MSG_IMAGE_LOADED, this.src );
 				self.trigger("imageLoaded", true);
+				getPixelDataArray( );
+				drawPixelatedToCanvas( );
 			};
 			image.onerror = function() {
 				message( MSG_TYPE_ERROR, MSG_IMAGE_LOAD_ERROR, this.src );
@@ -96,11 +169,13 @@
 			return !!(elem.getContext && elem.getContext('2d'));
 		}
 
-		function resizeCanvas() {
+		function resizeCanvases() {
 			if(options.resizeCanvas) {
 				canv.width = image.width;
 				canv.height = image.height;
 			}
+			tmpcanv.width = image.width;
+			tmpcanv.height = image.height;
 		}
 
 		function meetsRequirements() {
