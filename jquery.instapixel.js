8d2156bf0c9a5ed9f002f8555bde4a4d307cb827
Eric Howard <erichoward86@gmail.com>
2013-08-10 10:39:23 -0400

    v0.0.1 - intial plugin stub

---
+++ jquery.instapixel.js
@@ -0,0 +1,17 @@
+/***********************************************
+
+ InstaPIXEL jQuery Plugin
+ Author: 	Eric Howard
+ URL: 		http://www.thelucre.com
+ Veriosn:	v0.0.1	  
+
+ ***********************************************/
+(function($) {
+
+    $.fn.instaPIXEL = function() {
+
+        // Future home of "Hello, World!"
+
+    }
+
+}(jQuery));
